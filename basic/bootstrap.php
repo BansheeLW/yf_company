ed922909068baf141b6a91b50fdc1a4713fa56f2
BansheeLw <18813975696b@gmail.com>
2018-07-10 20:07:46 +0800

    index修改

---
+++ basic/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: banshee
+ * Date: 2018/7/10
+ * Time: 下午8:05
+ */
+Yii::setAlias('service', dirname(dirname(__DIR__)) . '/common');
+Yii::setAlias('helpers', dirname(dirname(__DIR__)) . '/helpers');
