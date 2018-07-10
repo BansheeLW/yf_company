671fe16daed18617500348388ca039ff88d03148
BansheeLw <18813975696b@gmail.com>
2018-07-10 21:32:24 +0800

    db test

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yf_company',
     'username' => 'root',
-    'password' => '',
+    'password' => 'wujia1314',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
