52ae23b4bbafb1cbbce4cc0eb95a6388bb68a194
BansheeLw <18813975696b@gmail.com>
2018-07-22 15:49:54 +0800

    版本更新1.0

--- basic/models/SearchForm.php
+++ basic/models/SearchForm.php
@@ -16,9 +16,13 @@ class SearchForm extends BaseFormModel {
     public $name;
     public $password;
     public $company_name;
+    public $version;
 
     const NAME = 'yf_company';
     const PASS = 'wujia1314';
+
+    const SCENARIO_GET_DATA = 'get_data';
+
 
     public static $areaList = [0=>'', 1=>'宝安区',2=>'罗湖区',3=>'龙华区',4=>'光明区',5=>'南山区',6=>'福田区',7=>'龙岗区',8=>'坪山区',9=>'大鹏区',10=>'盐田区',11=>'东莞',12=>'其他'];
     public static $typeList = [0=>'',1=>'A+：关系非常好的优质客户',2=>'A：关系很好',3=>'B：关系一般',4=>'C：无法跟进',5=>'D：黑名单'];
@@ -29,6 +33,7 @@ class SearchForm extends BaseFormModel {
     {
         return [
             [['name','password', 'company_name'], 'string'],
+            [['company_name','version'],'on'=>self::SCENARIO_GET_DATA],
         ];
     }
 
@@ -45,6 +50,9 @@ class SearchForm extends BaseFormModel {
     public function getData(){
         if(!$this->validate()){
             return $this->returnAndSetError(new ErrorInfo(['code'=>'-1','message'=>'参数错误']));
+        }
+        if($this->version != 1.0){
+            return $this->returnAndSetError(new ErrorInfo(['code'=>'-1','message'=>'旧版已废弃，请更新客户端版本']));
         }
         $result = Follow::getByCompanyName($this->company_name);
         foreach ($result as &$item) {
